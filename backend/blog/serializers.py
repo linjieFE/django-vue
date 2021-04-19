76c481a6c6655eb629585844f8158fdd88a60c59
linjie.zheng <linjie.zheng@flexiv.com>
2021-04-19 16:22:19 +0800

    创建viewset

--- backend/blog/serializers.py
+++ backend/blog/serializers.py
@@ -1,5 +1,6 @@
 form rest_framework import serializers
-
+from .models import Blog
 class BlogSerializer(serializers.HyperlinkedModelSerializer)
     class Meta:
         model = Blog
+        fields = '__all__'
